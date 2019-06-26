701b02bf28659b3996dd331005b4cbf967cb0674
Serhii Vabishchevych <sergiy.vabischevych@gmail.com>
2019-06-26 05:50:34 +0300

    complete task

--- src/app/core/store/effects/app.effects.ts
+++ src/app/core/store/effects/app.effects.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Observable } from 'rxjs';
-import { map, tap, withLatestFrom } from 'rxjs/operators';
+import { map, tap, withLatestFrom, filter, debounceTime } from 'rxjs/operators';
 import { Action, Store, select } from '@ngrx/store';
 
 import * as fromAppActions from '@app.store/actions/app.actions';
 import * as fromReducers from '@app.store/index';
 import { NumberCardModel, CardStatus } from '@app/models';
+import { isNgTemplate } from '@angular/compiler';
 
 const MATRIX_SIZE = 6;
 const MIN_NUMBER_VALUE = 1;
@@ -64,6 +65,31 @@ export class AppEffects {
     .pipe(
       ofType<fromAppActions.CompareNumberPairsAction>(fromAppActions.AppActionTypes.CompareShowedCards),
       map(action => action.payload.pairs),
+      filter((pairs: Array<NumberCardModel>) => pairs.filter((item: NumberCardModel) => item.status === CardStatus.Show).length === 2),
+      tap((pairs: Array<NumberCardModel>) => {
+        this.store.dispatch(new fromAppActions.SetNumberPairsAction({ pairs }));
+      }),
+      map((pairs: Array<NumberCardModel>) => {
+        const showedCard: Array<NumberCardModel> = pairs
+          .filter((item: NumberCardModel) => item.status === CardStatus.Show);
+        const isEqual: boolean = showedCard[0].value === showedCard[1].value;
+        return pairs.map((item: NumberCardModel) => {
+          if ( item.status === CardStatus.Show) {
+            return { ...item, status: isEqual ? CardStatus.Open : CardStatus.Close };
+          }
+          return item;
+        });
+      }),
+      debounceTime(500),
+      map((pairs: Array<NumberCardModel>) => new fromAppActions.SetNumberPairsAction({ pairs })),
+    );
+
+  @Effect()
+  changeCardStatus1$: Observable<Action> = this.actions$
+    .pipe(
+      ofType<fromAppActions.CompareNumberPairsAction>(fromAppActions.AppActionTypes.CompareShowedCards),
+      map(action => action.payload.pairs),
+      filter((pairs: Array<NumberCardModel>) => pairs.filter((item: NumberCardModel) => item.status === CardStatus.Show).length < 2),
       map((pairs: Array<NumberCardModel>) => new fromAppActions.SetNumberPairsAction({ pairs })),
     );
 
