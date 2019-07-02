5cd1a10cd92c9b4e9724f515ec28710225a3ca22
Serhii Vabishchevych <sergiy.vabischevych@gmail.com>
2019-07-02 18:32:00 +0300

    fix AppComponent unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed, async } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+
 import { AppComponent } from './app.component';
 import { NumberCardComponent } from './components';
+import { State } from '@app.store/index';
 
 describe('AppComponent', () => {
+  let store: MockStore<{ State }>;
+  const initialState: State = {
+    app: {
+      numberPairs: []
+    }
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         NumberCardComponent,
       ],
-      providers: [  ]
+      providers: [ provideMockStore({ initialState }) ]
     }).compileComponents();
+
+    store = TestBed.get(Store);
   }));
 
   it('should create the app', () => {
